d1b933d0328f153a4aa0275d5ee6af73a15584d7
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-07-02 21:29:08 +0800

    BasicLayout,少量注释版

--- log4cpp.cpp
+++ log4cpp.cpp
@@ -8,13 +8,16 @@
 #include "log4cpp/BasicLayout.hh"
 #include "log4cpp/Priority.hh"
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace log4cpp;
 using std::cout;
 using std::string;
+using std::ofstream;
 
 int main()
 {
+    //1.日志输出器
     Appender* appender1 = new OstreamAppender("console", &cout);  
     appender1->setLayout(new BasicLayout());
     
@@ -23,27 +26,34 @@ int main()
 
     Appender* appender3 = new FileAppender("log", "program.log");
     appender3->setLayout(new BasicLayout());
+
+    std::ofstream ofs("program2.log");
+    OstreamAppender * appender4 = new OstreamAppender("file", &ofs);
+    appender4->setLayout(new BasicLayout());
     
+    //2.日志记录器
     Category & root = Category::getRoot(); //用getRoot()创建根对象
-    root.setPriority(Priority::WARN); 
+    root.setPriority(Priority::WARN);      //设置日志优先级
     root.addAppender(appender1);
     
     Category & sub1 = Category::getInstance(string("sub1"));//创建叶对象
     sub1.addAppender(appender2);
     
-    Category & sub11 = Category::getInstance(string("sub1.sub11"));
-
     Category & sub2 = Category::getInstance(string("sub2"));
     sub2.addAppender(appender3);
+
+    Category & sub11 = Category::getInstance(string("sub1.sub11"));
+    sub11.setAdditivity(false); //不继承
+    sub11.addAppender(appender4);
 
     //输出日志:方式1
     root.error("root error");
     root.info("root info");
     sub1.error("sub1 error");
     sub1.warn("sub1 warn");
-    sub11.error("sub11 error");
     sub2.error("sub2 error");
     sub2.warn("sub2 warn");
+    sub11.error("sub11 error");
     
     //方式2
     sub11.warn("%d + %d == %s", 1, 1 ,"two");
