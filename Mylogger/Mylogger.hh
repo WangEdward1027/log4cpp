6398795020d1d5f7633fb25c89f5b77d8f07fd01
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-07-04 14:31:17 +0800

    海豹

---
+++ Mylogger/Mylogger.hh
@@ -0,0 +1,32 @@
+#ifndef __MyLogger_HH__
+#define __MyLogger_HH__
+
+#include <log4cpp/Category.hh>
+
+#define addPrefix(msg) string("[").append(__FILE__)\
+            .append(":").append(__func__)\
+            .append(":").append(std::to_string(__LINE__))\
+            .append("]").append(msg).c_str()
+
+#define LogWarn(msg) Mylogger::getInstance()->warn(addPrefix(msg))
+
+class Mylogger
+{
+public:
+	void warn(const char *msg);
+	void error(const char *msg);
+	void debug(const char *msg);
+	void info(const char *msg);
+
+    static Mylogger* getInstance();
+    static void destroy();
+private:
+	Mylogger();
+	~Mylogger();
+
+private:
+    log4cpp::Category & _mycat;
+    static Mylogger * _pInstance;
+};
+
+#endif
